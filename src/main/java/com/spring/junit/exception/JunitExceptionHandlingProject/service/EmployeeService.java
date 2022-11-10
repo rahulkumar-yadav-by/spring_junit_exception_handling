99c874e171974cf981a1c2e614914f57178b3e13
rahulkumar-yadav-by <rahulkumar.yadav@blueyonder.com>
2022-11-10 10:39:36 +0530

    Added Crud operations

---
+++ src/main/java/com/spring/junit/exception/JunitExceptionHandlingProject/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.spring.junit.exception.JunitExceptionHandlingProject.service;
+
+import com.spring.junit.exception.JunitExceptionHandlingProject.model.Employee;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EmployeeService {
+
+    List<Employee> getEmployees();
+
+    Employee getEmployeeById(Long id);
+
+    Employee saveEmployee(Employee employee);
+
+    List<Employee> saveAllEmployees(List<Employee> employees);
+
+    String removeEmployee(Long id);
+}
